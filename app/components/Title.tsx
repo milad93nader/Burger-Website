3036f6a85ab3b364fe52c4d56c7e2de6e924bab6
miladnader19933 <miladnader19933@gmail.com>
2024-06-14 12:32:41 +0300

    Donttouch

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -8,7 +8,7 @@ const Title = ( ) => {
     
 <div>
 <title>Burger</title>
-    <link rel="shortcut icon" type="image" href="../icon/logo.png"/>
+    <link rel="shortcut icon" type="image" href={shortcuticon.src}/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
 <Script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></Script>
 <Script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></Script>
